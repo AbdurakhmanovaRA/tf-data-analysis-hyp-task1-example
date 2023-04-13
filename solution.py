8fd186783889495381338b2e8ca7c88f0d76962f
Regina <48201923+AbdurakhmanovaRA@users.noreply.github.com>
2023-04-13 20:47:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 241769496 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
+    if (x_success >= x_cnt) and (y_success >= y_cnt):
+      flag = True
+    else:
+      flag = False
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return flag # Ваш ответ, True или False
